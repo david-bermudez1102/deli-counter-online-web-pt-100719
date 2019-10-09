b18a0aff2f3931283032cb47c815a3173fa480e0
David Bermudez <juan.bermudez1102@outlook.com>
2019-10-09 00:40:36 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,11 @@
-# Write your code here.
+def line(queue)
+  if(queue.length==0)
+    puts "The line is currently empty."
+  else
+    current_line = "The line is currently:"
+      queue.each.with_index { |person,index|  
+        current_line << " #{index}. #{person}"
+      }
+    puts current_line
+  end
+end
